106906b4f8e5686fe7bff01f47b72d31136d1f1e
Fabrice Chapot <fabrice.chapot@gmail.com>
2024-04-12 10:54:45 +0200

    Finalisation de la mise à zéro Newgame

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Liste des chemins d'accès aux images
+// Liste des images
 const images = [
   'assets/img/dice-1.png',
   'assets/img/dice-2.png',
@@ -10,33 +10,37 @@ const images = [
 
 let intervalId; // Pour stocker l'ID de l'intervalle
 let randomIndex; // Pour stocker l'index actuel
+let score = 0; // Pour stocker le chiffre du lancé
 let currentScore = 0; // Pour stocker le score total des lancés
 let total_p1 = 0 // Pour stocker le score total du player 1
 let total_p2 = 0 // Pour stocker le score total du player 2
 
+function playSound(soundName) {
+  const audio = new Audio(`assets/sound/${soundName}`);
+  audio.play();
+}
 
 // New Game
-
 let whoIsPlaying = 'p1';
 const newGameButton = document.getElementById('new-game');
 function newGame() {
-  const newGameSound = new Audio('assets/sound/newgame.m4a');
-  newGameSound.play();
+  playSound('newgame.m4a');
   whoIsPlaying = "p1";
-  currentScore = 0
-  player_2.classList.remove('container-player--activ');
-  player_1.classList.add('container-player--activ');
-  command_panel.classList.remove('container-conmmand__fond--activ');
-  affTotalP1.innerHTML = '0';
-  affTotalP2.innerHTML = '0';
+  affTotalP1.innerHTML = 0;
+  total_p1 = 0;
+  affTotalP2.innerHTML = 0;
+  total_p2 = 0;
   currentscore_p1.innerHTML = 0;
   currentscore_p2.innerHTML = 0;
   currentScore = 0;
+  score = 0;
+  player_2.classList.remove('container-player--activ');
+  player_1.classList.add('container-player--activ');
+  command_panel.classList.remove('container-conmmand__fond--activ');
 }
 newGameButton.addEventListener('click', newGame);
 
 // Bascule graphique
-
 const player_1 = document.getElementById('player-1');
 const player_2 = document.getElementById('player-2');
 const h1_player_1 = document.getElementById('h1-player-1');
@@ -62,30 +66,30 @@ function afficherImageAleatoire() {
 const dice = document.getElementById('dice');
 
 function demarrerDefilement() {
-  const desSound = new Audio('assets/sound/des4.m4a');
-  desSound.play();
+  playSound('des4.m4a');
   afficherImageAleatoire(); // Affiche une image aléatoire dès le début
   intervalId = setInterval(afficherImageAleatoire, 100);
   dice.classList.toggle('container-commands__dice--rotate');
   setTimeout(arreterDefilement, 1000);
 }
 
-  // Arrêter le défilement des images
-  function arreterDefilement() {
-    clearInterval(intervalId);
-    dice.classList.toggle('container-commands__dice--rotate');
-    score = randomIndex + 1;
-    calcCurrentScore();
-  }
+function arreterDefilement() {
+  clearInterval(intervalId);
+  dice.classList.toggle('container-commands__dice--rotate');
+  score = randomIndex + 1;
+  calcCurrentScore();
+}
+
+const rollDice = document.getElementById('rolldice-button');
+rollDice.addEventListener('click', demarrerDefilement);
 
   // Shake Score Panel
   const scorePanelP1 = document.getElementById('score-panel-p1');
   const scorePanelP2 = document.getElementById('score-panel-p2');
 
   function scorePanelShake() {
-    const holdSound = new Audio('assets/sound/SFB-caisse2.mp3');
     if (score != 1){
-    holdSound.play();
+      playSound('SFB-caisse2.mp3');
     }
     if(whoIsPlaying === 'p1') {
       scorePanelP1.classList.toggle('container-player__current-score-block--rotate');
@@ -102,7 +106,6 @@ function demarrerDefilement() {
  }
 
     // Stocker les points dans le total par joueur
-  
   let affTotalP1 = document.getElementById('total-p1');
   let affTotalP2 = document.getElementById('total-p2');
 
@@ -127,21 +130,16 @@ function demarrerDefilement() {
     setTimeout(whoIsWinner, 100);
   }
   
-    // Associer les boutons aux fonctions
   const holdButton = document.getElementById('hold-button')
   holdButton.addEventListener('click', holdScoreAndChange);
-
-  const rollDice = document.getElementById('rolldice-button');
-  rollDice.addEventListener('click', demarrerDefilement);
 
 // Calcul et Stockage du données
 let currentscore_p1 = document.getElementById('currentscore_p1');
 let currentscore_p2 = document.getElementById('currentscore_p2');
 
 function calcCurrentScore() {
-  const looseSound = new Audio('assets/sound/loose.mp3');
   if (score == 1){
-    looseSound.play();
+    playSound('loose.mp3')
     currentScore = 0;
     holdScoreAndChange();
   }
